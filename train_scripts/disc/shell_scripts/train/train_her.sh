67c7ec26ba3a076b82390d819df5aa0e21313a46
GongXudong <gongxudong_cs@aliyun.com>
2024-12-13 21:23:13 +0800

    disc: add configs for sac

--- train_scripts/disc/shell_scripts/train/train_her.sh
+++ train_scripts/disc/shell_scripts/train/train_her.sh
@@ -27,3 +27,17 @@ python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac
 python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_1_reg_0_0001/sac_config_10hz_128_128_seed_3.json
 python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_1_reg_0_0001/sac_config_10hz_128_128_seed_4.json
 python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_1_reg_0_0001/sac_config_10hz_128_128_seed_5.json
+
+# epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.1
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_0_1/sac_config_10hz_128_128_seed_1.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_0_1/sac_config_10hz_128_128_seed_2.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_0_1/sac_config_10hz_128_128_seed_3.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_0_1/sac_config_10hz_128_128_seed_4.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_0_1/sac_config_10hz_128_128_seed_5.json
+
+# epsilon = [1.0, 0.3, 0.3]  regularization_strength = 1.0
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_1/sac_config_10hz_128_128_seed_1.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_1/sac_config_10hz_128_128_seed_2.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_1/sac_config_10hz_128_128_seed_3.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_1/sac_config_10hz_128_128_seed_4.json
+python train_scripts/disc/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_1/sac_config_10hz_128_128_seed_5.json
--- train_scripts/disc/shell_scripts/train/train_ppo.sh
+++ train_scripts/disc/shell_scripts/train/train_ppo.sh
@@ -27,3 +27,17 @@ python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo
 python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_1_reg_0_001/ppo_bc_config_10hz_128_128_seed_3.json
 python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_1_reg_0_001/ppo_bc_config_10hz_128_128_seed_4.json
 python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_1_reg_0_001/ppo_bc_config_10hz_128_128_seed_5.json
+
+# epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.1
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_0_1/ppo_bc_config_10hz_128_128_seed_1.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_0_1/ppo_bc_config_10hz_128_128_seed_2.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_0_1/ppo_bc_config_10hz_128_128_seed_3.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_0_1/ppo_bc_config_10hz_128_128_seed_4.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_0_1/ppo_bc_config_10hz_128_128_seed_5.json
+
+# epsilon = [1.0, 0.3, 0.3]  regularization_strength = 1.0
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_1/ppo_bc_config_10hz_128_128_seed_1.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_1/ppo_bc_config_10hz_128_128_seed_2.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_1/ppo_bc_config_10hz_128_128_seed_3.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_1/ppo_bc_config_10hz_128_128_seed_4.json
+python train_scripts/disc/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_1/ppo_bc_config_10hz_128_128_seed_5.json
