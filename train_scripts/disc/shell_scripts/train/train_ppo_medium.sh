03c4e58bcd96a04ec9251bc7794e5e6d7237c22c
GongXudong <gongxudong_cs@aliyun.com>
2024-12-27 11:24:15 +0800

    disc: add ppo configs

--- train_scripts/disc/shell_scripts/train/train_ppo_medium.sh
+++ train_scripts/disc/shell_scripts/train/train_ppo_medium.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+#---------------------------------------- epsilon = [0.1, 0.03, 0.03] -------------------------------------------------------------
+# epsilon = [0.1, 0.03, 0.03]  regularization_strength = 0.0001  N=16
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_2.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_3.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_4.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_5.json
+
+# epsilon = [0.1, 0.03, 0.03]  regularization_strength = 0.001  N=16
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_2.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_3.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_4.json
+python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_5.json
+
+
 #---------------------------------------- epsilon = [1.0, 0.3, 0.3] -------------------------------------------------------------
 # epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0
 python train_scripts/disc/train/train_ppo.py --config-file-name configs/train/disc/ppo/medium/epsilon_0_1_reg_0/128_128_seed_1.json
--- train_scripts/disc/shell_scripts/train/train_sac.sh
+++ train_scripts/disc/shell_scripts/train/train_sac.sh
@@ -2,6 +2,13 @@
 
 
 #---------------------------------------- epsilon = [0.1, 0.03, 0.03] -------------------------------------------------------------
+# epsilon = [0.1, 0.03, 0.03]  regularization_strength = 0.0001  noise_num = 16
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_2.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_3.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_4.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_5.json
+
 # epsilon = [0.1, 0.03, 0.03]  regularization_strength = 0.001  noise_num = 16
 python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_1.json
 python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_001_N_16/128_128_seed_2.json
