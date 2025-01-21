db0942b3ee03ecb008a4659e884db4ab32db0614
GongXudong <gongxudong_cs@aliyun.com>
2025-01-22 00:06:05 +0800

    disc: add configs for HER

--- train_scripts/disc/shell_scripts/evaluate/her/N_16/evaluate_her_by_success_rate.sh
+++ train_scripts/disc/shell_scripts/evaluate/her/N_16/evaluate_her_by_success_rate.sh
@@ -2,6 +2,10 @@
 
 # bash train_scripts/disc/shell_scripts/evaluate/her/N_16/evaluate_her_by_success_rate.sh &> tmp_her_res.txt
 
+
+
+
+
 #---------------------------------------- epsilon = [1.0, 0.3, 0.3] -------------------------------------------------------------
 # epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.001  N = 16
 python train_scripts/disc/evaluate/evaluate_policy_by_success_rate.py --algo-config-file configs/train/disc/her/medium2/epsilon_0_1_reg_0_001_N_16/128_128_seed_1.json --env-config-file configs/env/D2D/env_config_for_sac_medium2_b_05.json --algo sac --seed 6745 --n-envs 32 --n-eval-episode 1000
--- train_scripts/disc/shell_scripts/train/train_sac.sh
+++ train_scripts/disc/shell_scripts/train/train_sac.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+python train_scripts/train_with_rl_sac_her.py --config-file-name configs/train/sac/medium_without_her/sac_config_10hz_128_128_1.json
+python train_scripts/train_with_rl_sac_her.py --config-file-name configs/train/sac/medium_without_her/sac_config_10hz_128_128_2.json
+python train_scripts/train_with_rl_sac_her.py --config-file-name configs/train/sac/medium_without_her/sac_config_10hz_128_128_3.json
+python train_scripts/train_with_rl_sac_her.py --config-file-name configs/train/sac/medium_without_her/sac_config_10hz_128_128_4.json
+python train_scripts/train_with_rl_sac_her.py --config-file-name configs/train/sac/medium_without_her/sac_config_10hz_128_128_5.json
+
+
 #---------------------------------------- epsilon = [0.1, 0.03, 0.03] -------------------------------------------------------------
 # epsilon = [0.1, 0.03, 0.03]  regularization_strength = 0.0001  noise_num = 16
 python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_01_reg_0_0001_N_16/128_128_seed_1.json
