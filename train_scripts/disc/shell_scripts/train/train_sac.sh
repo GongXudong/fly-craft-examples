c26e1d2c775d1405f7d68edd665395f8f20d52ae
GongXudong <gongxudong_cs@aliyun.com>
2025-02-06 16:24:22 +0800

    disc: add configs

--- train_scripts/disc/shell_scripts/train/train_sac.sh
+++ train_scripts/disc/shell_scripts/train/train_sac.sh
@@ -89,6 +89,13 @@ python train_scripts/disc/train/train_sac.py --config-file-name configs/train/di
 python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/epsilon_0_1_reg_1_N_16/128_128_seed_5.json
 
 
+# epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.0  beta = 0.0  noise_num = 16
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0/epsilon_0_1_reg_0_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0/epsilon_0_1_reg_0_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0/epsilon_0_1_reg_0_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0/epsilon_0_1_reg_0_N_16/128_128_seed_1.json
+python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0/epsilon_0_1_reg_0_N_16/128_128_seed_1.json
+
 # epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.0001  beta = 0.0001  noise_num = 16
 python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0_0001/epsilon_0_1_reg_0_0001_N_16/128_128_seed_1.json
 python train_scripts/disc/train/train_sac.py --config-file-name configs/train/disc/sac/medium/beta_0_0001/epsilon_0_1_reg_0_0001_N_16/128_128_seed_2.json
