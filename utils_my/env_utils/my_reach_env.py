3c0e2d4f1a30015db7a22ae16165d8c8204b94ad
Sen1553 <485333926@qq.com>
2025-03-10 23:04:18 +0800

    add configs

---
+++ utils_my/env_utils/my_reach_env.py
@@ -0,0 +1,67 @@
+from typing import Optional
+
+import numpy as np
+
+from panda_gym.envs.core import RobotTaskEnv
+from panda_gym.envs.robots.panda import Panda
+from panda_gym.envs.tasks.flip import Flip
+from panda_gym.envs.tasks.pick_and_place import PickAndPlace
+from panda_gym.envs.tasks.push import Push
+#from panda_gym.envs.tasks.reach import Reach
+from utils_my.env_utils.task.my_reach_task import ChangeRewardReach
+from panda_gym.envs.tasks.slide import Slide
+from panda_gym.envs.tasks.stack import Stack
+from panda_gym.pybullet import PyBullet
+
+
+class MyPandaReachEnv(RobotTaskEnv):
+    """Reach task wih Panda robot.
+
+    Args:
+        render_mode (str, optional): Render mode. Defaults to "rgb_array".
+        reward_type (str, optional): "sparse" or "dense". Defaults to "sparse".
+        control_type (str, optional): "ee" to control end-effector position or "joints" to control joint values.
+            Defaults to "ee".
+        renderer (str, optional): Renderer, either "Tiny" or OpenGL". Defaults to "Tiny" if render mode is "human"
+            and "OpenGL" if render mode is "rgb_array". Only "OpenGL" is available for human render mode.
+        render_width (int, optional): Image width. Defaults to 720.
+        render_height (int, optional): Image height. Defaults to 480.
+        render_target_position (np.ndarray, optional): Camera targetting this postion, as (x, y, z).
+            Defaults to [0., 0., 0.].
+        render_distance (float, optional): Distance of the camera. Defaults to 1.4.
+        render_yaw (float, optional): Yaw of the camera. Defaults to 45.
+        render_pitch (float, optional): Pitch of the camera. Defaults to -30.
+        render_roll (int, optional): Rool of the camera. Defaults to 0.
+    """
+
+    def __init__(
+        self,
+        render_mode: str = "rgb_array",
+        reward_type: str = "sparse",
+        control_type: str = "ee",
+        renderer: str = "Tiny",
+        render_width: int = 720,
+        render_height: int = 480,
+        render_target_position: Optional[np.ndarray] = None,
+        render_distance: float = 1.4,
+        render_yaw: float = 45,
+        render_pitch: float = -30,
+        render_roll: float = 0,
+        goal_range: float = 0.1, 
+        distance_threshold: float = 0.01
+    ) -> None:
+        sim = PyBullet(render_mode=render_mode, renderer=renderer)
+        robot = Panda(sim, block_gripper=True, base_position=np.array([-0.6, 0.0, 0.0]), control_type=control_type)
+        task = ChangeRewardReach(sim, reward_type=reward_type, get_ee_position=robot.get_ee_position, distance_threshold=distance_threshold, goal_range=goal_range)
+        super().__init__(
+            robot,
+            task,
+            render_width=render_width,
+            render_height=render_height,
+            render_target_position=render_target_position,
+            render_distance=render_distance,
+            render_yaw=render_yaw,
+            render_pitch=render_pitch,
+            render_roll=render_roll,
+        )
+
