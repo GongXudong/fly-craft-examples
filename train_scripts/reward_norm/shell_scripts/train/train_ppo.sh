b9ff5b1dca0d868a60b889be015ff3167e7aa54a
GongXudong <gongxudong_cs@aliyun.com>
2025-03-11 09:47:50 +0800

    add configs

--- train_scripts/reward_norm/shell_scripts/train/train_ppo.sh
+++ train_scripts/reward_norm/shell_scripts/train/train_ppo.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # baseline
-python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/128_128_seed_1.json
-python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/128_128_seed_2.json
-python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/128_128_seed_3.json
-python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/128_128_seed_4.json
-python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/128_128_seed_5.json
+
+# end-to-end mode
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/baseline/128_128_seed_1.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/baseline/128_128_seed_2.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/baseline/128_128_seed_3.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/baseline/128_128_seed_4.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_end_to_end/baseline/128_128_seed_5.json
+
+# guidance-law mode
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_guidance/baseline/128_128_seed_1.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_guidance/baseline/128_128_seed_2.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_guidance/baseline/128_128_seed_3.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_guidance/baseline/128_128_seed_4.json
+python train_scripts/reward_norm/train/train_with_rl_ppo.py --config-file-name configs/train/reward_norm/ppo/medium_guidance/baseline/128_128_seed_5.json
--- train_scripts/reward_norm/shell_scripts/train/train_sac.sh
+++ train_scripts/reward_norm/shell_scripts/train/train_sac.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # baseline 
-python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/128_128_seed_1.json
-python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/128_128_seed_2.json
-python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/128_128_seed_3.json
-python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/128_128_seed_4.json
-python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/128_128_seed_5.json
+
+# end-to-end mode
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/baseline/128_128_seed_1.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/baseline/128_128_seed_2.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/baseline/128_128_seed_3.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/baseline/128_128_seed_4.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_end_to_end/baseline/128_128_seed_5.json
+
+# guidance-law mode
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_guidance/baseline/128_128_seed_1.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_guidance/baseline/128_128_seed_2.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_guidance/baseline/128_128_seed_3.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_guidance/baseline/128_128_seed_4.json
+python train_scripts/reward_norm/train/train_with_rl_sac_her.py --config-file-name configs/train/reward_norm/sac/medium_guidance/baseline/128_128_seed_5.json
