d267fdca4355cd4857ea73cace352e93edd509d7
Sen1553 <485333926@qq.com>
2025-04-11 00:10:18 +0800

    add bash

--- train_scripts/D2D/shells/F2F/run_F2F_single_stage.sh
+++ train_scripts/D2D/shells/F2F/run_F2F_single_stage.sh
@@ -13,3 +13,18 @@ CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stag
 CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_3.json
 CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_4.json
 CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_5.json
+
+# skip 3 to skip 1  1e6
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_3_skip_1/sac_config_10hz_128_128_1.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_3_skip_1/sac_config_10hz_128_128_2.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_3_skip_1/sac_config_10hz_128_128_3.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_3_skip_1/sac_config_10hz_128_128_4.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_3_skip_1/sac_config_10hz_128_128_5.json
+
+
+# skip 5 to skip 1  1e6
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_5_skip_1/sac_config_10hz_128_128_1.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_5_skip_1/sac_config_10hz_128_128_2.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_5_skip_1/sac_config_10hz_128_128_3.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_5_skip_1/sac_config_10hz_128_128_4.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/two_stage_skip_5_skip_1/sac_config_10hz_128_128_5.json
