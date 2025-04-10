b89783232ebbf79e43b5f350ddcdb2bf6969df9c
Sen1553 <485333926@qq.com>
2025-04-10 11:26:14 +0800

    add F2F train shell

---
+++ train_scripts/D2D/shells/F2F/run_F2F_single_stage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# skip3 1e6
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_3/sac_config_10hz_128_128_1.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_3/sac_config_10hz_128_128_2.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_3/sac_config_10hz_128_128_3.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_3/sac_config_10hz_128_128_4.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_3/sac_config_10hz_128_128_5.json
+
+# skip5 1e6
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_1.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_2.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_3.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_4.json
+CUDA_VISIBLE_DEVICES=0 python train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/F2F/relative_hard/b_05/skip_5/sac_config_10hz_128_128_5.json
