739571a4ea38fe8647ee4d6bd30dfa81ade0343e
Sen1553 <485333926@qq.com>
2025-04-24 15:28:28 +0800

    add config

--- train_scripts/D2D/shells/reach/run_reach_distance0005.sh
+++ train_scripts/D2D/shells/reach/run_reach_distance0005.sh
@@ -131,3 +131,50 @@ python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --con
 python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/two_stage_n_substeps_5_to_n_substeps_1/b_1/sac_seed_3.json
 python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/two_stage_n_substeps_5_to_n_substeps_1/b_1/sac_seed_4.json
 python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/two_stage_n_substeps_5_to_n_substeps_1/b_1/sac_seed_5.json
+
+
+
+# D2D
+## reach dense sac d2d  b = 1 100
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_1/sac_seed_1.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_1/sac_seed_2.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_1/sac_seed_3.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_1/sac_seed_4.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_1/sac_seed_5.json
+
+## reach dense sac d2d  b = 2  110
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_2/sac_seed_1.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_2/sac_seed_2.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_2/sac_seed_3.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_2/sac_seed_4.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_2/sac_seed_5.json
+
+## reach dense sac d2d  b = 0.5  242
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_05/sac_seed_1.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_05/sac_seed_2.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_05/sac_seed_3.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_05/sac_seed_4.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_05/sac_seed_5.json
+
+## reach dense sac d2d  b = 0.25  166
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_025/sac_seed_1.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_025/sac_seed_2.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_025/sac_seed_3.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_025/sac_seed_4.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_025/sac_seed_5.json
+
+
+## reach dense sac d2d  b = 0.125 10
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_0125/sac_seed_1.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_0125/sac_seed_2.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_0125/sac_seed_3.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_0125/sac_seed_4.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/b_0125/sac_seed_5.json
+
+
+## reach dense sac d2d  b = 2 to b = 0.5  120
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/two_stage_b2_b05/sac_seed_1.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/two_stage_b2_b05/sac_seed_2.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/two_stage_b2_b05/sac_seed_3.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/two_stage_b2_b05/sac_seed_4.json
+python train_scripts/D2D/train_with_rl_sac_her_multi_stages_panda_reach.py --config-file-name=configs/train/D2D/pand_reach_dense/goal_range_08/distance_0_01_change_z_scope/dense/sac/D2D/two_stage_b2_b05/sac_seed_5.json
