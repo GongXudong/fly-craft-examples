4d52619106e2fd59bf11feb0d500249dc3860018
Sen1553 <485333926@qq.com>
2025-05-01 11:19:59 +0800

    add config

--- train_scripts/D2D/shells/VVC_SAC_D2D/run_goal_space.sh
+++ train_scripts/D2D/shells/VVC_SAC_D2D/run_goal_space.sh
@@ -9,6 +9,14 @@
 # python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/goal_sapce/easy_to_medium/sac_config_10hz_128_128_5.json
 
 
+# goal space easy to medium  b = 0.5 242
+python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/goal_sapce/easy_to_medium/b_05/sac_config_10hz_128_128_1.json
+python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/goal_sapce/easy_to_medium/b_05/sac_config_10hz_128_128_2.json
+python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/goal_sapce/easy_to_medium/b_05/sac_config_10hz_128_128_3.json
+python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/goal_sapce/easy_to_medium/b_05/sac_config_10hz_128_128_4.json
+python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/goal_sapce/easy_to_medium/b_05/sac_config_10hz_128_128_5.json
+
+
 # easy sac  b=1 120
 # python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/easy_sac/b_1/sac_config_10hz_128_128_1.json
 # python  train_scripts/D2D/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/D2D/easy_sac/b_1/sac_config_10hz_128_128_2.json
