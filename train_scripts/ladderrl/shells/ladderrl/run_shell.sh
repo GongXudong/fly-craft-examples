ab56efbe9ae3862fd9017f8f8d17f9125ef67d2f
Sen1553 <485333926@qq.com>
2025-05-15 21:04:07 +0800

    add configs

--- train_scripts/ladderrl/shells/ladderrl/run_shell.sh
+++ train_scripts/ladderrl/shells/ladderrl/run_shell.sh
@@ -24,3 +24,12 @@ python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-fil
 python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/sac_config_10hz_128_128_3.json
 python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/sac_config_10hz_128_128_4.json
 python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/sac_config_10hz_128_128_5.json
+
+
+
+# primacy_bias  baseline  buffersize 1e6  gradient step 4  242
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step4/sac_config_10hz_128_128_1.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step4/sac_config_10hz_128_128_2.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step4/sac_config_10hz_128_128_3.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step4/sac_config_10hz_128_128_4.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step4/sac_config_10hz_128_128_5.json
