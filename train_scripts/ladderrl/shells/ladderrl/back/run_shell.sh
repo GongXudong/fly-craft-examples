2f6358bde9e923f3fa1d5aa233b67486c359933f
Sen1553 <485333926@qq.com>
2025-05-24 15:42:36 +0800

    add config

--- train_scripts/ladderrl/shells/ladderrl/back/run_shell.sh
+++ train_scripts/ladderrl/shells/ladderrl/back/run_shell.sh
@@ -64,3 +64,26 @@ python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-fil
 python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/sac_config_10hz_128_128_3.json
 python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/sac_config_10hz_128_128_4.json
 python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/sac_config_10hz_128_128_5.json
+
+
+#easy buffersize 1e6 primacy bias reset 2e5  gradient 1 warmup 300  
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/warmup300/sac_config_10hz_128_128_1.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/warmup300/sac_config_10hz_128_128_2.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/warmup300/sac_config_10hz_128_128_3.json
+# python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/warmup300/sac_config_10hz_128_128_4.json
+# python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/easy/sac/b_1/primacy_bias_1e6_buffer_1e6/gradient_step1/warmup300/sac_config_10hz_128_128_5.json
+
+
+#medium buffersize 1e6 primacy bias reset 2e5  gradient 1 warmup 300  
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/gradient_step1/warmup300/sac_config_10hz_128_128_1.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/gradient_step1/warmup300/sac_config_10hz_128_128_2.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/gradient_step1/warmup300/sac_config_10hz_128_128_3.json
+# python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/gradient_step1/warmup300/sac_config_10hz_128_128_4.json
+# python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/gradient_step1/warmup300/sac_config_10hz_128_128_5.json
+
+#medium buffersize 2e6 primacy bias reset 4e5  gradient 1 warmup 300  
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/warmp300/sac_config_10hz_128_128_1.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/warmp300/sac_config_10hz_128_128_2.json
+python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/warmp300/sac_config_10hz_128_128_3.json
+# python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/warmp300/sac_config_10hz_128_128_4.json
+# python train_scripts/ladderrl/train_with_rl_sac_her_multi_stages.py --config-file-name=configs/train/ladderrl/baseline/medium/sac/b_1/buffersize_2e6_reset4e5/warmp300/sac_config_10hz_128_128_5.json
