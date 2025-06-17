91eac104cedc9c6156452a350040cea31a0dffea
GongXudong <gongxudong_cs@aliyun.com>
2025-06-17 10:03:00 +0800

    modify path import: flycraft.utils -> flycraft.utils_common

--- train_scripts/VVCGym/test/test_policy.py
+++ train_scripts/VVCGym/test/test_policy.py
@@ -24,7 +24,7 @@ from imitation.data import rollout
 
 import flycraft
 from flycraft.env import FlyCraftEnv
-from flycraft.utils.load_config import load_config
+from flycraft.utils_common.load_config import load_config
 
 PROJECT_ROOT_DIR = Path(__file__).parent.parent
 if str(PROJECT_ROOT_DIR.absolute()) not in sys.path:
