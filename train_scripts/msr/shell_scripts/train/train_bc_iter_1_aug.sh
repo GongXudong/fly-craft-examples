4ebb9ebfd0485d315b235008caccafe572753ba2
GongXudong <gongxudong_cs@aliyun.com>
2025-06-24 10:35:25 +0800

    add configs

--- train_scripts/msr/shell_scripts/train/train_bc_iter_1_aug.sh
+++ train_scripts/msr/shell_scripts/train/train_bc_iter_1_aug.sh
@@ -37,6 +37,14 @@ python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_1/128_128_seed_5.json
 
 
+
+# epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.0001  N=16
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_0001_N_16/128_128_seed_1.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_0001_N_16/128_128_seed_2.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_0001_N_16/128_128_seed_3.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_0001_N_16/128_128_seed_4.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_0001_N_16/128_128_seed_5.json
+
 # epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.001  N=16
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_001_N_16/128_128_seed_1.json
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/epsilon_0_1_reg_0_001_N_16/128_128_seed_2.json
