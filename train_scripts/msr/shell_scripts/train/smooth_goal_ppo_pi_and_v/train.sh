a4b1256cfb2bca652013c91430b9681b085a78cc
GongXudong <gongxudong_cs@aliyun.com>
2025-06-26 15:48:16 +0800

    add configs

--- train_scripts/msr/shell_scripts/train/smooth_goal_ppo_pi_and_v/train.sh
+++ train_scripts/msr/shell_scripts/train/smooth_goal_ppo_pi_and_v/train.sh
@@ -51,3 +51,10 @@ python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr
 python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_10_v_beta_0/128_128_seed_3.json
 python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_10_v_beta_0/128_128_seed_4.json
 python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_10_v_beta_0/128_128_seed_5.json
+
+## 1.8 v_regularization_strength = 100.0  v_beta = 0
+python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_100_v_beta_0/128_128_seed_1.json
+python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_100_v_beta_0/128_128_seed_2.json
+python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_100_v_beta_0/128_128_seed_3.json
+python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_100_v_beta_0/128_128_seed_4.json
+python train_scripts/msr/train/train_ppo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi_and_v/medium/epsilon_0_1_reg_0_001_beta_0_N_16/v_reg_100_v_beta_0/128_128_seed_5.json
