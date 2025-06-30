39ce66d130b0afb0986a81f92c99ae128d37aaaf
GongXudong <gongxudong_cs@aliyun.com>
2025-06-30 16:02:11 +0800

    add configs about msr-bc

---
+++ train_scripts/msr/shell_scripts/evaluate/bc/iter_3_aug/evaluate_bc_with_average_adjacent_kl.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+# Iter 3
+# algo: epsilon 0.0 reg 0.0  eval: epsilon 0.01
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class PPO --algo-ckpt-dir checkpoints/IRPO/bc/guidance_law_mode/iter_3/256_256_128_128_64_300epochs_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str PPO --algo-epsilon 0.0 --algo-reg 0.0 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 0.01 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_reg_0_noise_0_01.csv
+
+# algo: epsilon 0.0 reg 0.0  eval: epsilon 0.1
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class PPO --algo-ckpt-dir checkpoints/IRPO/bc/guidance_law_mode/iter_3/256_256_128_128_64_300epochs_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str PPO --algo-epsilon 0.0 --algo-reg 0.0 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 0.1 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_reg_0_noise_0_1.csv
+
+# algo: epsilon 0.0 reg 0.0  eval: epsilon 1.0
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class PPO --algo-ckpt-dir checkpoints/IRPO/bc/guidance_law_mode/iter_3/256_256_128_128_64_300epochs_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str PPO --algo-epsilon 0.0 --algo-reg 0.0 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 1.0 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_reg_0_noise_1.csv
+
+
+#--------------------------------------------------------------------SmoothGoalBC  Iter 3  epsilon=0.1--------------------------------------------------------------
+
+# algo: epsilon 0.1 reg 0  eval: epsilon 0.01  noise_num: 16
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class SmoothGoalPPO --algo-ckpt-dir checkpoints/msr/hard/bc/iter_3_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_300epochs_seed_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str SmoothGoalPPO --algo-epsilon 0.1 --algo-reg 0.0 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 0.01 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_1_reg_0_N_16_noise_0_01.csv
+
+# algo: epsilon 0.1 reg 0  eval: epsilon 0.1  noise_num: 16
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class SmoothGoalPPO --algo-ckpt-dir checkpoints/msr/hard/bc/iter_3_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_300epochs_seed_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str SmoothGoalPPO --algo-epsilon 0.1 --algo-reg 0.0 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 0.1 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_1_reg_0_N_16_noise_0_1.csv
+
+# algo: epsilon 0.1 reg 0  eval: epsilon 1.0  noise_num: 16
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class SmoothGoalPPO --algo-ckpt-dir checkpoints/msr/hard/bc/iter_3_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_300epochs_seed_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str SmoothGoalPPO --algo-epsilon 0.1 --algo-reg 0.0 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 1.0 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_1_reg_0_N_16_noise_1.csv
+
+
+
+# algo: epsilon 0.1 reg 0.0001  eval: epsilon 0.01  noise_num: 16
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class SmoothGoalPPO --algo-ckpt-dir checkpoints/msr/hard/bc/iter_3_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_300epochs_seed_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str SmoothGoalPPO --algo-epsilon 0.1 --algo-reg 0.0001 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 0.01 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_1_reg_0_0001_N_16_noise_0_01.csv
+
+# algo: epsilon 0.1 reg 0.0001  eval: epsilon 0.1  noise_num: 16
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class SmoothGoalPPO --algo-ckpt-dir checkpoints/msr/hard/bc/iter_3_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_300epochs_seed_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str SmoothGoalPPO --algo-epsilon 0.1 --algo-reg 0.0001 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 0.1 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_1_reg_0_0001_N_16_noise_0_1.csv
+
+# algo: epsilon 0.1 reg 0.0001  eval: epsilon 1.0  noise_num: 16
+python train_scripts/msr/evaluate/evaluate_ppo_with_average_adjacent_KL.py --env-config configs/env/VVCGym/env_hard_config_for_sac.json --env-flag-str Hard-05 --algo-class SmoothGoalPPO --algo-ckpt-dir checkpoints/msr/hard/bc/iter_3_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_300epochs_seed_{0} --algo-ckpt-model-name bc_checkpoint --algo-seeds 1 2 3 4 5 --algo-flag-str SmoothGoalPPO --algo-epsilon 0.1 --algo-reg 0.0001 --evaluate-dg-num 100 --evaluate-noise-base 10.0 3.0 3.0 --evaluate-noise-multiplier 1.0 --evaluate-adjacent-num 5 --res-file-save-name train_scripts/msr/plots/bc/results/bc_iter_3_epsilon_0_1_reg_0_0001_N_16_noise_1.csv
+
+
--- train_scripts/msr/shell_scripts/train/train_bc_iter_4_aug.sh
+++ train_scripts/msr/shell_scripts/train/train_bc_iter_4_aug.sh
@@ -8,12 +8,24 @@ python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/128_128_seed_4.json
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/128_128_seed_5.json
 
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_seed_1.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_seed_2.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_seed_3.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_seed_4.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_N_16/256_256_128_128_64_seed_5.json
+
 # epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.0001  N=16
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/128_128_seed_1.json
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/128_128_seed_2.json
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/128_128_seed_3.json
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/128_128_seed_4.json
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/128_128_seed_5.json
+
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_seed_1.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_seed_2.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_seed_3.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_seed_4.json
+python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_0001_N_16/256_256_128_128_64_seed_5.json
 
 # epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.001  N=16
 python train_scripts/msr/train/train_bc.py --config-file-name configs/train/msr/bc/hard/iter_4_aug/epsilon_0_1_reg_0_001_N_16/128_128_seed_1.json
