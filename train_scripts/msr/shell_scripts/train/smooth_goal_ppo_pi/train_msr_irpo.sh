8fe72f6b13c6ffa062205f4c2801e0ab009cf914
GongXudong <gongxudong_cs@aliyun.com>
2025-08-25 11:56:08 +0800

    debug

--- train_scripts/msr/shell_scripts/train/smooth_goal_ppo_pi/train_msr_irpo.sh
+++ train_scripts/msr/shell_scripts/train/smooth_goal_ppo_pi/train_msr_irpo.sh
@@ -2,9 +2,9 @@
 
 
 # 1.IRPO: lambda = 0.001   MSR: epsilon = [1.0, 0.3, 0.3]  regularization_strength = 0.001  beta = 0  N = 16
-python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/epsilon_0_1_reg_0_001_N_16/128_128_seed_1.json
-python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/epsilon_0_1_reg_0_001_N_16/128_128_seed_2.json
-python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/epsilon_0_1_reg_0_001_N_16/128_128_seed_3.json
-python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/epsilon_0_1_reg_0_001_N_16/128_128_seed_4.json
-python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/epsilon_0_1_reg_0_001_N_16/128_128_seed_5.json
+python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/irpo_lambda_0_001_msr_epsilon_0_1_reg_0_001_N_16/128_128_seed_1.json
+python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/irpo_lambda_0_001_msr_epsilon_0_1_reg_0_001_N_16/128_128_seed_2.json
+python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/irpo_lambda_0_001_msr_epsilon_0_1_reg_0_001_N_16/128_128_seed_3.json
+python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/irpo_lambda_0_001_msr_epsilon_0_1_reg_0_001_N_16/128_128_seed_4.json
+python train_scripts/msr/train/from_bc/train_ppo_msr_irpo.py --config-file-name configs/train/msr/smooth_goal_ppo_pi/hard/from_bc_with_irpo/irpo_lambda_0_001_msr_epsilon_0_1_reg_0_001_N_16/128_128_seed_5.json
 
